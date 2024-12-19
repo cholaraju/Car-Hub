03d62a2da32b0be105c5a47aba90dabcb007ae7d
ThePrimeagen <the.primeagen@aol.com>
2024-12-19 11:04:47 +0530

    added-ignorebuilds

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: [
+    
+      'cdn.imagin.studio'
+    ]
+  },
+  typescript: {
+  ignoreBuildErrors: true,
+  
+  }
+};
+
+export default nextConfig;
